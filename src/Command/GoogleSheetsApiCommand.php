2d7e8d9730543075ffa17c2cbbb3a1c70ba90434
tacman <tacman@users.noreply.github.com>
2026-05-12 14:09:14 +0000

    refactor all commands to remove execute(), new contracts, etc.

--- src/Command/GoogleSheetsApiCommand.php
+++ src/Command/GoogleSheetsApiCommand.php
@@ -5,66 +5,53 @@ namespace Survos\GoogleSheetsBundle\Command;
 use Survos\GoogleSheetsBundle\Service\GoogleApiClientService;
 use Survos\GoogleSheetsBundle\Service\GoogleSheetsApiService;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand('googlesheets:execute',"Wrapper for console commands")]
-class GoogleSheetsApiCommand extends Command
+#[AsCommand('googlesheets:execute', 'Wrapper for console commands')]
+final class GoogleSheetsApiCommand
 {
-
     public function __construct(
         private readonly GoogleApiClientService $clientService,
         private readonly GoogleSheetsApiService $googleSheetsApiService,
-    )
-    {
-        parent::__construct();
-    }
-    protected function configure(): void
-    {
-        $this
-        ->addOption('function', null, InputOption::VALUE_OPTIONAL,'sheets api function to be executed')
-        ->addOption('title', null, InputOption::VALUE_OPTIONAL,'sheet title in string')
-        ->addOption('id', null, InputOption::VALUE_OPTIONAL,'spreadsheets id')
-        ->addOption('header', null, InputOption::VALUE_OPTIONAL,'number of rows for the header', 0)
-        ->addOption('data', null, InputOption::VALUE_OPTIONAL,'grid data in 2 dimensional array');
+    ) {
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $function = $input->getOption('function');
-        $id = $input->getOption('id');
-        $sheetTitle = $input->getOption('title');
-        $data = json_decode((string) $input->getOption('data'));
-        $header = $input->getOption('header');
-
-        $clientService = $this->clientService;
+    public function __invoke(
+        SymfonyStyle $io,
+        #[Option('Sheets API function to execute')] ?string $function = null,
+        #[Option('Sheet title')] ?string $title = null,
+        #[Option('Spreadsheet ID')] ?string $id = null,
+        #[Option('Number of rows for the header')] int $header = 0,
+        #[Option('Grid data as JSON')] ?string $data = null,
+    ): int {
+        $parsedData = $data !== null ? json_decode($data) : null;
         $service = $this->googleSheetsApiService;
 
         $response = 'no action has been made';
-        if($function == 'token') {
-            $response = $clientService->createNewSheetApiAccessToken();
+        if ($function === 'token') {
+            $response = $this->clientService->createNewSheetApiAccessToken();
         } else {
             $service->setSheetServices($id);
         }
 
-        if($function == 'get') {
+        if ($function === 'get') {
             $service->setSheetServices($id);
             $response = $service->getGoogleSpreadSheets();
             dd($response);
-        } elseif($function == 'create') {
-            $response = $service->createNewSheet($sheetTitle, $data, $header);
-        } elseif($function == 'update') {
-            $response = $service->updateSheet($sheetTitle, $data, $header);
-        } elseif($function == 'clear') {
-            $response = $service->clearSheetByTitle($sheetTitle);
-        } elseif($function == 'delete') {
-            $response = $service->deleteSheetByTitle($sheetTitle);
+        } elseif ($function === 'create') {
+            $response = $service->createNewSheet($title, $parsedData, $header);
+        } elseif ($function === 'update') {
+            $response = $service->updateSheet($title, $parsedData, $header);
+        } elseif ($function === 'clear') {
+            $response = $service->clearSheetByTitle($title);
+        } elseif ($function === 'delete') {
+            $response = $service->deleteSheetByTitle($title);
         }
 
-        $output->writeln($response);
+        $io->writeln((string) $response);
 
-        return self::SUCCESS;
+        return Command::SUCCESS;
     }
 }
